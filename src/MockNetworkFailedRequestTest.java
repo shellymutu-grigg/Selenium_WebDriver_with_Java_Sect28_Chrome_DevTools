205329ab8b44d0f8edaaa994d9e945fae73ac2a9
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-07 09:09:06 +1200

    Section 28 update for lesson 223

--- src/MockNetworkFailedRequestTest.java
+++ src/MockNetworkFailedRequestTest.java
@@ -4,13 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v114.fetch.Fetch;
 import org.openqa.selenium.devtools.v114.fetch.model.RequestPattern;
+import org.openqa.selenium.devtools.v114.network.model.ErrorReason;
 
-public class MockNetworkBehaviourTest {
+public class MockNetworkFailedRequestTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// Setup driver
@@ -23,13 +25,8 @@ public class MockNetworkBehaviourTest {
 		// Create session
 		devTools.createSession();
 
-		// Enable the fetch object
-		devTools.send(Fetch.enable(Optional.empty(), Optional.empty()));
-
-		System.out.println(MessageFormat.format("After Fetch.enable: {0}", devTools.hashCode()));
-
 		Optional<List<RequestPattern>> patterns = Optional
-				.of(Arrays.asList(new RequestPattern(Optional.of("*"), Optional.empty(), Optional.empty())));
+				.of(Arrays.asList(new RequestPattern(Optional.of("*GetBook*"), Optional.empty(), Optional.empty())));
 
 		// Enable the fetch object
 		devTools.send(Fetch.enable(patterns, Optional.empty()));
@@ -40,21 +37,7 @@ public class MockNetworkBehaviourTest {
 		devTools.addListener(Fetch.requestPaused(), request -> {
 			System.out.println(MessageFormat.format("Inside Fetch.requestPaused(): {0}", devTools.hashCode()));
 
-			if (request.getRequest().getUrl().contains("=shetty")) {
-				//
-				// Create mocked URL
-				String mockedUrl = request.getRequest().getUrl().replace("=shetty", "=BadGuy");
-				System.out.println(MessageFormat.format("Mocked URL: {0}", mockedUrl));
-
-				// Inject mocked URL into network call
-				devTools.send(Fetch.continueRequest(request.getRequestId(), Optional.of(mockedUrl), Optional.empty(),
-						Optional.empty(), Optional.empty(), Optional.empty()));
-			} else {
-
-				// Use original URL into network call
-				devTools.send(Fetch.continueRequest(request.getRequestId(), Optional.of(request.getRequest().getUrl()),
-						Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
-			}
+			devTools.send(Fetch.failRequest(request.getRequestId(), ErrorReason.FAILED));
 		});
 
 		// Access webpage
@@ -62,19 +45,17 @@ public class MockNetworkBehaviourTest {
 
 		// Click on library button
 		chromeDriver.findElement(By.cssSelector("button[routerlink*='library']")).click();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-		System.out.println(MessageFormat.format("Paragraph text in libarary page: {0}",
-				chromeDriver.findElement(By.cssSelector("p")).getText()));
+		Thread.sleep(3000);
+
+		String paragraphMessageString = "";
+		List<WebElement> size = chromeDriver.findElements(By.cssSelector("p"));
+		if (size.isEmpty()) {
+			paragraphMessageString = "Houston we have success! No text displayed!";
+		}
+		System.out.println(MessageFormat.format("Paragraph text in libarary page: {0}", paragraphMessageString));
 
 		// Close session
 		chromeDriver.close();
-
 	}
-
 }
